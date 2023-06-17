412ed2c3d50e29a579cb9c3c904f1cc66986a8c0
ShuaiWei <54382045+Sw511@users.noreply.github.com>
2023-06-17 21:58:51 +0800

    Delete stl/fight_Sw511 directory

--- stl/fight_Sw511/README.md
+++
@@ -1,9 +0,0 @@
-# Team
-
-Team Name: fight_Sw511
-
-Team Member: Sw511
-
-## Other
-
-...
--- stl/fight_Sw511/Sw511/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  std::cout << std::endl;
-  return 0;
-}
