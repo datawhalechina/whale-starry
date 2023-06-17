e2d2d7bb09363ef0ca9c6ad406b8250c5b433f31
wangzhongren <wangzhongren@sjtu.edu.cn>
2023-06-17 20:17:55 +0800

    Feature: testing hello world.
    change the name and format
    
    Author: wangzhongren87@gmail.com

--- stl/test/wangzhongren/src/hello_world.cpp
+++ stl/test/wangzhongren/src/hello_world.cpp
@@ -3,15 +3,14 @@
  * @Email: wangzhongren87@gmail.com
  * @Date: 2023-06-17 17:13:59
  * @LastEditor: wangzhongren
- * @LastEditTime: 2023-06-17 17:24:48
+ * @LastEditTime: 2023-06-17 20:17:34
  * @Discription: the testing code for helloword!
  */
 
 #include <iostream>
 using namespace std;
  
-int main() 
-{
+int main() {
     cout << "Hello, World!" << endl ;
     return 0;
 }
