e1dc86d0a26975b229dacd04779f5b9f3d140d98
TimE9527 <1178168173@qq.com>
2023-06-17 23:47:18 +0800

    Feature: Adjusted code format
    Indent 2 spaces.
    Author: 1178168173@qq.com.

--- stl/time/time/src/main.cpp
+++ stl/time/time/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string welcome = "Hello World!";
-    std::cout << "Hello World" << std::endl;
-    return 0;
+  std::string welcome = "Hello World!";
+  std::cout << "Hello World" << std::endl;
+  return 0;
 }
