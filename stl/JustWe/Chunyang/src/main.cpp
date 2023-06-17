a18600bd3f563e979cdab5e20256dbaa5275efaf
zhaochunyang9 <zhaochunyang9.jd.com>
2023-06-17 18:59:39 +0800

    Feature: Add hollo world

---
+++ stl/JustWe/Chunyang/src/main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    // Hollow world
+    std::cout << "Hollow world!" << std::endl;
+}
---
+++ stl/JustWe/README.md
@@ -0,0 +1,9 @@
+# Team
+
+Team Name: Just We
+
+Team Member: Chunyang
+
+## Other
+
+...
