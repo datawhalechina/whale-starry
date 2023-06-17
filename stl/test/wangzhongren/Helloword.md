ce3f2c8da8d24de0d102af768c55a3f49a435f5a
wangzhongren <wangzhongren@sjtu.edu.cn>
2023-06-17 17:35:36 +0800

    Feature: testing hello world.
    the hello world code!
    
    Author: wangzhongren87@gmail.com

---
+++ stl/test/wangzhongren/Helloword.md
@@ -0,0 +1,3 @@
+# Testing Hello World!
+
+## This is a testing code for helloworld!
---
+++ stl/test/wangzhongren/src/helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: wangzhongren
+ * @Email: wangzhongren87@gmail.com
+ * @Date: 2023-06-17 17:13:59
+ * @LastEditor: wangzhongren
+ * @LastEditTime: 2023-06-17 17:24:48
+ * @Discription: the testing code for helloword!
+ */
+
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    cout << "Hello, World!" << endl ;
+    return 0;
+}
