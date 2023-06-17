72d1ec78b102eb9b8d5b7e46247b7bd6f8b84cd2
“Topwoo” <1244807501@qq.com>
2023-06-17 23:05:05 +0800

    edit in google style

--- stl/kuafu/triplewo/src/00-hello_world/main.cpp
+++ stl/kuafu/triplewo/src/00-hello_world/main.cpp
@@ -1,9 +1,8 @@
-#include<iostream>
+#include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-  string welcome = "Hello World!";
+  std::string welcome = "Hello World!";
   std::cout << welcome << std::endl;
   system("pause");
   return 0;
