cca35616e1623f88cb4e6660e084158bdd693896
light-city <455954986@qq.com>
2023-06-18 21:18:19 +0800

    test

---
+++ stl/fight_flow/light-city/src/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::vector<char> data;
+  std::string str = "Hello, World!";
+  for (char c : str) {
+    data.push_back(c);
+  }
+    for (char c : data) 
+    {
+    std::cout << c;
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
