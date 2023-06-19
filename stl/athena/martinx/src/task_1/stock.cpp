675e9c69655eab47d6d9cc0e4eebe4976b0580b2
Martin Xu <martin.xus@gmail.com>
2023-06-20 01:45:46 +0800

    [athena_1]: notes & code for task_1: stl_list

---
+++ stl/athena/martinx/src/task_1/stock.cpp
@@ -0,0 +1,83 @@
+#include "stock.h"
+
+#include <iostream>
+
+#include "httplib.h"
+#include "simdjson.h"
+
+// 使用股票代码初始化
+Stock::Stock(const std::string& code) { this->code = code; };
+
+// 获取股票最后一个交易日的分钟K线数据
+std::string Stock::GetData() {
+  std::string fields = "time,price,volume,avg_price,amount,highest,lowest";
+
+  // 创建 HTTP 客户端
+  httplib::Client cli("yunhq.sse.com.cn", 32041);
+
+  // 发送请求
+  auto res = cli.Get("/v1/sh1/line/" + this->code +
+                     "?callback=&select=" + fields + "&_=1687183788521");
+
+  // 如果请求成功，打印结果
+  if (res && res->status == 200) {
+    return res->body;
+  } else {
+    std::cout << "error code: " << res->status << std::endl;
+    return "{}";
+  }
+}
+
+// 解析JSON数据，获取K线数据列表
+std::list<StockData> Stock::ParseData(std::string& data) {
+  auto res_body_padded = simdjson::padded_string(this->HandleData(data));
+
+  // 解析 string 为 json
+  simdjson::ondemand::parser parser;
+  simdjson::ondemand::document json = parser.iterate(res_body_padded);
+  std::list<StockData> values;
+
+  // 获取json中的line数组，并遍历
+  for (auto line : json["line"].get_array()) {
+    auto item = line.get_array();
+    // 获取K线时间
+    // int64_t time = item.at(0).get_int64();
+    // times.push_back(time);
+
+    // 获取当前价格
+    StockData stock_data = StockData();
+    stock_data.close = item.at(1).get_double();
+    values.push_back(stock_data);
+
+    // 获取成交量
+    // std::cout << "volume: " << item.at(2) << std::endl;
+    // 获取均价
+    // std::cout << "avg_price: " << item.at(3) << std::endl;
+    // 获取成交额
+    // std::cout << "amount: " << item.at(4) << std::endl;
+    // 获取最高价
+    // std::cout << "highest: " << item.at(5) << std::endl;
+    // 获取最低价
+    // std::cout << "lowest: " << item.at(6) << std::endl;
+  }
+
+  return values;
+}
+
+// 打印结果
+void Stock::PrintData(const std::list<StockData>& stocks) {
+  for (auto stock : stocks) {
+    std::cout << "price: " << stock.close << std::endl;
+  }
+}
+
+// 处理数据
+std::string Stock::HandleData(std::string& data) {
+  if (data != "{}") {
+    int data_size = data.size();
+    std::string res_body = data.erase(0, 1).erase(data_size - 2, data_size - 1);
+    return res_body;
+  }
+
+  return "{}";
+}
