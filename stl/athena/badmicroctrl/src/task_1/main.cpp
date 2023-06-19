8bfccb9a958546eb04d248797347ec3c9f14cc1c
badmicroctrl <badmicroctrl@163.com>
2023-06-19 20:08:50 +0800

    Feature: main.cpp
    learn the container 'list' and write some code using list
    
    Author: badmicroctrl@163.com

--- stl/athena/badmicroctrl/src/task_1/main.cpp
+++ stl/athena/badmicroctrl/src/task_1/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <list>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  list<int> mylist;
+  mylist.push_back(1);
+  mylist.push_back(2);
+  mylist.push_back(3);
+  mylist.push_front(0);
+  for (auto it = mylist.begin(); it != mylist.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  mylist.remove(2);
+  for (auto it = mylist.begin(); it != mylist.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+
   return 0;
 }
