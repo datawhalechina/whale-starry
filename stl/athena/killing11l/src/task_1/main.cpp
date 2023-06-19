8cdf65e87df5715cc3661097f4aa744492196527
feiwei <nothing.feiwei@gmail.com>
2023-06-19 08:19:44 +0000

    Feature: reading std::list.
    Read the std::list related implementation and perform a simple interface call exercise.
    
    Author: nothing.feiwei@gmail.com
    Author: badmicroctrl@163.com
    Author: nothing.feiwei@gmail.com
    Author: 208360449@qq.com.
    Author: 208360449@qq.com.
    Author: ivarnd@qq.com.
    Author: invarn-d

--- stl/athena/killing11l/src/task_1/main.cpp
+++ stl/athena/killing11l/src/task_1/main.cpp
@@ -1,6 +1,50 @@
+#include <functional>
 #include <iostream>
+#include <list>
+#include <optional>
+
+// 事件类型
+using Event = std::function<void()>;
+
+// 事件调度程序
+class EventDispatcher {
+ public:
+  // 添加事件
+  template <typename F>
+  void AddEvent(F&& f) {
+    events_.emplace_back(std::forward<F>(f));
+  }
+
+  // 处理事件
+  void Dispatch() {
+    while (!events_.empty()) {
+      // 获取下一个事件
+      auto event = std::move(events_.front());
+      events_.pop_front();
+
+      // 处理事件
+      if (event) {
+        std::invoke(*event);
+      }
+    }
+  }
+
+ private:
+  std::list<std::optional<Event>> events_;
+};
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  // 创建事件调度程序
+  EventDispatcher dispatcher;
+
+  // 添加多个事件
+  int a = 1, b = 2;
+  dispatcher.AddEvent([&a, &b]() { std::cout << "a + b = " << a + b << std::endl; });
+  dispatcher.AddEvent([&a, &b]() { std::cout << "a * b = " << a * b << std::endl; });
+  dispatcher.AddEvent([&a, &b]() { std::cout << "a - b = " << a - b << std::endl; });
+
+  // 处理事件
+  dispatcher.Dispatch();
+
   return 0;
 }
---
+++ stl/daydayup/ivarn-d/README.md
@@ -0,0 +1,5 @@
+##  ivarn-d 的学习笔记
+
+| 序号 | 任务                                                         | 开始时间   | 结束时间   | 笔记                         | 代码                                                         |
+| ---- | ------------------------------------------------------------ | ---------- | ---------- | ---------------------------- | ------------------------------------------------------------ |
+| 1    | [小试牛刀-list](https://github.com/gcc-mirror/gcc/blob/releases/gcc-9/libstdc%2B%2B-v3/include/bits/stl_list.h) | 2023-06-19 | 2023-06-19 | [list](./doc/task_1/List.md) | [ivarnd_list.h](./src/task_1/main.cpp) [main.cpp](./srdc/task_1/main.cpp) |
