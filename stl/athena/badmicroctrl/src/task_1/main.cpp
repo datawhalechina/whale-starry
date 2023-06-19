a7f8bca04c2af2905d53c1784d45f1a226895bdd
badmicroctrl <badmicroctrl@163.com>
2023-06-19 21:59:01 +0800

    Feature: main.cpp
    delete using namespace std
    
    Author: badmicroctrl@163.com

--- stl/athena/badmicroctrl/src/task_1/main.cpp
+++ stl/athena/badmicroctrl/src/task_1/main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <list>
-using namespace std;
 
 int main() {
-  list<int> mylist;
+  std::list<int> mylist;
   mylist.push_back(1);
   mylist.push_back(2);
   mylist.push_back(3);
   mylist.push_front(0);
   for (auto it = mylist.begin(); it != mylist.end(); ++it) {
-    cout << *it << " ";
+    std::cout << *it << " ";
   }
-  cout << endl;
+  std::cout << std::endl;
   mylist.remove(2);
   for (auto it = mylist.begin(); it != mylist.end(); ++it) {
-    cout << *it << " ";
+    std::cout << *it << " ";
   }
-  cout << endl;
+  std::cout << std::endl;
 
   return 0;
 }
