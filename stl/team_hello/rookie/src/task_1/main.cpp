f4fbfbc27f02581b5fcf975b16d49a6f4e88da86
wangzhenkai31 <wangzhenkai31@gmail.com>
2023-06-20 11:29:47 +0800

    Fix: modify main.cpp

--- stl/team_hello/rookie/src/task_1/main.cpp
+++ stl/team_hello/rookie/src/task_1/main.cpp
@@ -1,36 +1,36 @@
+#include <algorithm>
 #include <iostream>
 #include <list>
-#include <algorithm>
 
 int main() {
-    std::list<int> List;
+  std::list<int> m_list;
 
-    List.push_back(4);
-    List.push_back(5);
-    List.push_back(6);
-    List.push_front(3);
-    List.push_front(2);
-    List.push_front(1);
+  m_list.push_back(4);
+  m_list.push_back(5);
+  m_list.push_back(6);
+  m_list.push_front(3);
+  m_list.push_front(2);
+  m_list.push_front(1);
 
-    std::cout << "Init:" << std::endl;
-    std::for_each(List.begin(), List.end(), [](int data){
-        std::cout << data << " ";
-    });
-    std::cout << std::endl;
+  std::cout << "Init:" << std::endl;
+  for (const auto& data : m_list) {
+    std::cout << data << " ";
+  }
+  std::cout << std::endl;
 
-    List.insert(++List.begin(), 10);
-    std::cout << "Insert:" << std::endl;
-    std::for_each(List.begin(), List.end(), [](int data){
-        std::cout  << data << " ";
-    });
-    std::cout << std::endl;
+  m_list.insert(++m_list.begin(), 10);
+  std::cout << "Insert:" << std::endl;
+  for (const auto& data : m_list) {
+    std::cout << data << " ";
+  };
+  std::cout << std::endl;
 
-    std::cout << "Delete:" << std::endl;
-    List.erase(std::find(List.begin(), List.end(), 4));
-    std::for_each(List.begin(), List.end(), [](int data){
-        std::cout << data << " ";
-    });
-    std::cout << std::endl;
+  std::cout << "Delete:" << std::endl;
+  m_list.erase(std::find(m_list.begin(), m_list.end(), 4));
+  for (const auto& data : m_list) {
+    std::cout << data << " ";
+  };
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
