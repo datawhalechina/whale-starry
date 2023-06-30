da632b323d3e70036bd360a1dcd9956ad12fc9c5
SheldonLoveCoding <liuxd_sheldon@163.com>
2023-07-01 00:23:11 +0800

    Fix: modify my_set.hpp. remove redundant header files and modify variable name. Author: liuxd_sheldon@163.com

--- stl/cpp_on_shore/sheldon/src/task_3/my_set.hpp
+++ stl/cpp_on_shore/sheldon/src/task_3/my_set.hpp
@@ -1,34 +1,35 @@
 /*
  * 利用stl_tree.h实现set的自定义接口
  */
-#include <bits/concept_check.h>
+
 #include <bits/stl_tree.h>
+
 #include <initializer_list>
 
 template <typename _Key, typename _Compare = std::less<_Key>,
           typename _Alloc = std::allocator<_Key>>
 class my_set {
  private:
-  std::_Rb_tree<_Key, _Key, std::_Identity<_Key>, _Compare, _Alloc> itree;
-  std::_Rb_tree_iterator<_Key> iter = itree.begin();
+  std::_Rb_tree<_Key, _Key, std::_Identity<_Key>, _Compare, _Alloc> rbtree;
+  std::_Rb_tree_iterator<_Key> iter = rbtree.begin();
 
  public:
   void insert(_Key x) {
     /*插入 利用_M_insert_unique的特性，不用判断是否重复*/
-    itree._M_insert_unique(x);
+    rbtree._M_insert_unique(x);
   }
 
   int size() {
-    // _M_impl; declared protected in itree, so the below command doesn't work
-    // return itree._M_impl._M_node_count;
-    return itree.size();
+    // _M_impl; declared protected in rbtree, so the below command doesn't work
+    // return rbtree._M_impl._M_node_count;
+    return rbtree.size();
   }
 
   // 一些自定义的接口
   void print() {
     /*打印set*/
-    iter = itree.begin();
-    while (iter != itree.end()) {
+    iter = rbtree.begin();
+    while (iter != rbtree.end()) {
       std::cout << *iter << " ";
       iter++;
     }
@@ -37,18 +38,18 @@ class my_set {
 
   bool exist(_Key x) {
     /*返回 x 是否存在*/
-    if (itree.find(x) != itree.end()) {
+    if (rbtree.find(x) != rbtree.end()) {
       return true;
     } else {
       return false;
     }
   }
 
-  bool empty() { return itree.empty(); }
+  bool empty() { return rbtree.empty(); }
 
-  _Key max() { return *itree.rbegin(); }
+  _Key max() { return *rbtree.rbegin(); }
 
-  _Key min() { return *itree.begin(); }
+  _Key min() { return *rbtree.begin(); }
 
-  void erase(_Key x) { itree.erase(x); }
+  void erase(_Key x) { rbtree.erase(x); }
 };
