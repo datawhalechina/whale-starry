9244776a07ce302a4f162fb7a4794442a895aeda
曹志宾 <caozhibin@Christophers-MacBook-Pro.local>
2023-07-08 18:21:18 +0800

    Fearure: Adding some codes and notes to the demo adding... Author: 936481843@qq.com

--- stl/cpp_on_shore/chris/src/task_5/combination.cpp
+++ stl/cpp_on_shore/chris/src/task_5/combination.cpp
@@ -1,63 +1,8 @@
 #include "combination.h"
 
-void PrintVector(std::vector<ShoppingOrder*> orderVector) {
-  for (const auto& order : orderVector) {
-    std::cout << "orderNumber: " << order->GetOrderNumber() << "\n"
-              << "customerName: " << order->GetCustomerName() << "\n"
-              << "status: " << order->GetStatus() << std::endl;
-    std::cout << std::endl;
-  }
-  std::cout << "************************Done*****************************" << std::endl;
-}
+#include <iostream>
 
 int main() {
-  std::vector<ShoppingOrder*> orderVector;
-  std::list<ShoppingOrder*> orderList;
-  ShoppingOrder* order1 = new ShoppingOrder(1, "Alice", "Pending");
-  orderVector.emplace_back(order1);
-
-  // use list to realize a container that is similar to a stack
-  orderList.push_front(order1);
-
-  ShoppingOrder* order2 = new ShoppingOrder(2, "Bob", "Shipped");
-  orderVector.emplace_back(order2);
-  orderList.push_front(order2);
-
-  ShoppingOrder* order3 = new ShoppingOrder();
-  // Here is a Shallow Copy.
-  order3 = order2;
-  order3->SetOrderNumber(3);
-  order3->SetCustomerName("Chris");
-  // When we change the number of order3, order2 has also been changed.
-
-  orderVector.push_back(order3);
-  orderList.push_front(order3);
-
-  std::cout << "orderNumber: " << order1->GetOrderNumber() << "\n"
-            << "customerName: " << order1->GetCustomerName() << "\n"
-            << "status: " << order1->GetStatus() << std::endl;
-  std::cout << std::endl;
-
-  for (const auto& order : orderList) {
-    std::cout << "orderNumber: " << order->GetOrderNumber() << "\n"
-              << "customerName: " << order->GetCustomerName() << "\n"
-              << "status: " << order->GetStatus() << std::endl;
-    std::cout << std::endl;
-  }
-
-  /*
-  for (const auto& order : orderVector) {
-   std::cout << "orderNumber: " << order->GetOrderNumber() << "\n"
-    << "customerName: " << order->GetCustomerName() << "\n"
-    << "status: " << order->GetStatus() << std::endl;
-   std::cout << std::endl;
-  }
-  */
-  PrintVector(orderVector);
-
-  delete order1;
-  delete order2;
-  // delete order3;
-
-  std::cout << "Hello World!\n";
+  OrderManagementSys oms;
+  std::cout << "Hello STL!\n";
 }
